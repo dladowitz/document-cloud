6925c103ef430161383cd9411a8bd425d92d0a9d
David Ladowitz <david@ladowitz.com>
2013-03-07 00:04:01 -0800

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Documentcloud::Application.config.secret_token = '865ee53ac8db99b3aef18a18b7056e6c33f2186d6dc39d736beee1f62d8bc20a4d811c408e701b5e5a2fa25e2324964efa2bb4edec5916d9b43b827c736cbb6d'
